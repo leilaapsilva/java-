249ff8e77218bbf3bbaceff39c8b6f1ae2f9b2b5
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-06-05 17:39:23 -0300

    Create PrimeiroPrograma.java

---
+++ PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package primeiroprograma;
+/**
+ *
+ * @author Leila
+ */
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+       System.out.println("Olá, Mundo!");
+    }    
+}
