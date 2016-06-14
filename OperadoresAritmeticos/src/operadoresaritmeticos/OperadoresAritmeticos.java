f2f59fa0e2e9e80d6e998a2dd732c79d7230b5c6
Leila Aparecida da Silva <leilaapsilva@outlook.com.br>
2016-06-14 18:50:12 -0300

    Add files via upload

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Leila
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        System.out.println("A média é igual a " + m);
+        
+        /*int numero = 5;
+        numero++;
+        System.out.println(numero); */
+        
+        /*
+        //pós incremento        
+        int numero = 5;
+        int valor = 5 + numero++;
+        System.out.println(valor); */
+        
+        /*
+        //pré incremento        
+        int numero = 5;
+        int valor = 5 + ++numero;
+        System.out.println(valor); */
+        
+        /*
+        //pós decremento
+        int numero = 10;
+        int valor = 4 + numero--;
+        System.out.println(valor);
+        System.out.println(numero); */
+        
+        int x = 4;
+        x +=2; // x = x + 2
+        System.out.println(x);
+        
+        
+    }
+    
+}
